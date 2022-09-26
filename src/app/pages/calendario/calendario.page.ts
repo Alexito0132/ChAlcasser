326213e7908bb36cb5d3c041aa6e2c6a669d881e
Alexito0132 <alexitobt01@gmail.com>
2022-09-26 09:54:16 +0200

    fix(calendar): select day change month

--- src/app/pages/calendario/calendario.page.ts
+++ src/app/pages/calendario/calendario.page.ts
@@ -5,6 +5,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Day } from 'src/app/models/days';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-calendario',
@@ -17,14 +19,14 @@ export class CalendarioPage implements OnInit {
   monthDays: Day[];
   groupMonthDays: Array<Day[]>;
   rows = 0;
-  lastEvent;
+  lastEvent: Day;
 
   monthNumber: number;
   year: number;
 
   weekDaysName: string[] = [];
 
-  constructor() {
+  constructor(private translate: TranslateService) {
     const date = new Date();
     this.currentYear = date.getFullYear();
     this.currentMonthIndex = date.getMonth();
@@ -43,6 +45,10 @@ export class CalendarioPage implements OnInit {
 
     this.rows = Math.ceil(this.monthDays.length / 7);
     this.groupMonthDays = this.chunckArrayInGroups(this.monthDays, 7);
+    const date = new Date();
+
+    const index = this.getIndexOf(this.groupMonthDays, date.getDate());
+    this.addClass(this.groupMonthDays[index[0]][index[1]]);
   }
 
   public getCurrentMonth(): Day[] {
@@ -73,35 +79,35 @@ export class CalendarioPage implements OnInit {
     return days;
   }
 
-  public getMonthName(monthIndex: number): string {
+  public getMonthName(monthIndex: number): Observable<any> {
     switch (monthIndex + 1) {
       case 1:
-        return 'January';
+        return this.translate.get('calendar.month.january');
       case 2:
-        return 'February';
+        return this.translate.get('calendar.month.february');
       case 3:
-        return 'March';
+        return this.translate.get('calendar.month.march');
       case 4:
-        return 'April';
+        return this.translate.get('calendar.month.april');
       case 5:
-        return 'May';
+        return this.translate.get('calendar.month.may');
       case 6:
-        return 'June';
+        return this.translate.get('calendar.month.june');
       case 7:
-        return 'July';
+        return this.translate.get('calendar.month.july');
       case 8:
-        return 'August';
+        return this.translate.get('calendar.month.august');
       case 9:
-        return 'September';
+        return this.translate.get('calendar.month.september');
       case 10:
-        return 'October';
+        return this.translate.get('calendar.month.october');
       case 11:
-        return 'November';
+        return this.translate.get('calendar.month.november');
       case 12:
-        return 'December';
+        return this.translate.get('calendar.month.december');
 
       default:
-        return '|' + monthIndex;
+        return of('|' + monthIndex);
     }
   }
 
@@ -127,20 +133,30 @@ export class CalendarioPage implements OnInit {
     }
   }
 
-  addClass(event?: MouseEvent) {
-    console.log(this.lastEvent);
-    if (this.lastEvent === undefined) {
-      this.lastEvent = event.target;
-      this.lastEvent.classList.add('selected');
+  addClass(day: Day) {
+    if (this.lastEvent) {
+      this.lastEvent.selected = false;
+      this.lastEvent = day;
+      day.selected = true;
     } else {
-      this.lastEvent.classList.remove('selected');
-      console.log(this.lastEvent);
-
-      if (event !== undefined) {
-        this.lastEvent = event.target;
-      }
-      this.lastEvent.classList.add('selected');
+      this.lastEvent = day;
+      day.selected = true;
     }
+  }
+
+  searchDay(day: Day) {
+    if (day === undefined) {
+      return;
+    }
+    let index = this.getIndexOf(this.groupMonthDays, day.number);
+
+    while (index === undefined) {
+      // eslint-disable-next-line id-blacklist
+      day.number -= 1;
+      index = this.getIndexOf(this.groupMonthDays, day.number);
+    }
+
+    this.addClass(this.groupMonthDays[index[0]][index[1]]);
   }
 
   onNextMonth(): void {
@@ -154,7 +170,7 @@ export class CalendarioPage implements OnInit {
     this.setMonthDays(this.getMonth(this.monthNumber, this.year));
     this.rows = Math.ceil(this.monthDays.length / 7);
     this.groupMonthDays = this.chunckArrayInGroups(this.monthDays, 7);
-    this.addClass();
+    this.searchDay(this.lastEvent);
   }
 
   onPreviousMonth(): void {
@@ -168,7 +184,7 @@ export class CalendarioPage implements OnInit {
     this.setMonthDays(this.getMonth(this.monthNumber, this.year));
     this.rows = Math.ceil(this.monthDays.length / 7);
     this.groupMonthDays = this.chunckArrayInGroups(this.monthDays, 7);
-    this.addClass();
+    this.searchDay(this.lastEvent);
   }
 
   chunckArrayInGroups(arr, size): Array<Day[]> {
@@ -179,11 +195,20 @@ export class CalendarioPage implements OnInit {
     return chunk;
   }
 
+  getIndexOf(arr: Array<Day[]>, day: number) {
+    for (let i = 0; i < arr.length; i++) {
+      const index = arr[i].findIndex((d) => d.number === day);
+      if (index > -1) {
+        return [i, index];
+      }
+    }
+  }
+
   private createDay(dayNumber: number, monthIndex: number, year: number) {
     const day = new Day();
 
     day.monthIndex = monthIndex;
-    day.month = this.getMonthName(monthIndex);
+    this.getMonthName(monthIndex).subscribe((data) => (day.month = data));
 
     // eslint-disable-next-line id-blacklist
     day.number = dayNumber;
@@ -191,6 +216,7 @@ export class CalendarioPage implements OnInit {
 
     day.weekDayNumber = new Date(year, monthIndex, dayNumber).getDay();
     day.weekDayName = this.getWeekDayName(day.weekDayNumber);
+    day.selected = false;
 
     return day;
   }
